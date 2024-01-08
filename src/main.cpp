4ec2b1db7b4a51ef84ee6b01c30dd89233380038
Reynald Havard <reynald.havard@gmail.com>
2024-01-08 23:22:36 +0000

    add delay, concat result

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,39 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 
-int main(int argc, char** argv){
-    cv::VideoCapture capture("../data/vtest.avi");
-    if (!capture.isOpened()){
-        std::cerr << "Unable to open file!" << std::endl;
-        return 0;
-    }
-
-    while(true){
-        cv::Mat frame;
-        capture >> frame;
-        if (frame.empty()){
-            break;
-        }
-
-        cv::imshow("frame", frame);
-
-        int keyboard = cv::waitKey(30);
-        if (keyboard == 'q' || keyboard == 27){
-            break;
-        }
-    }
-
-    capture.release();
-
-    cv::destroyAllWindows();
-
+int main(int argc, char **argv) {
+  cv::VideoCapture cap("../data/vtest.avi");
+  if (!cap.isOpened()) {
+    std::cerr << "Unable to open file!" << std::endl;
     return 0;
+  }
+
+  double fps = cap.get(cv::CAP_PROP_FPS);
+  int delay = static_cast<int>(1000 / fps);
+
+  while (true) {
+    cv::Mat frame;
+    cap >> frame;
+    if (frame.empty()) {
+      break;
+    }
+
+    cv::Mat res = cv::Mat::zeros(frame.size(), frame.type());
+    // Process
+
+    // Compare original frame and foreground
+    cv::Mat displayMat;
+    cv::hconcat(frame, res, displayMat);
+    cv::imshow("display", displayMat);
+
+    int keyboard = cv::waitKey(delay);
+    if (keyboard == 'q' || keyboard == 27) {
+      break;
+    }
+  }
+
+  cap.release();
+  cv::destroyAllWindows();
+
+  return 0;
 }
